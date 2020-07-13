63e7beb2e5cf1c7f601787e942d65b71b13c86ad
Piotr Maślankowski <pmaslankowski@advatech.pl>
2020-07-13 23:03:46 +0200

    Color validation in turtle added.

--- rushing_turtles/model/turtle.py
+++ rushing_turtles/model/turtle.py
@@ -1,12 +1,19 @@
 
+COLORS = ['RED', 'BLUE', 'GREEN', 'YELLOW', 'PURPLE']
+
 class Turtle(object):
   color : str
 
   def __init__(self, color):
+    if color not in COLORS:
+      raise ValueError(f'Invalid color: {color}')
     self.color = color
 
-  def __str__(self):
-    return self.color
-  
+  def __eq__(self, other):
+    return self.color == other.color
+
+  def __hash__(self):
+    return hash(self.color)
+    
   def __repr__(self):
     return self.color
